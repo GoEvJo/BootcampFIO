5190fa0b55232454c8fc37d3df78a4ae71c72a22
Joaquín G <gonzalez.joaquin.ev@gmail.com>
2022-05-11 16:21:02 -0300

    Primer commit

---
+++ Clase5/main/Ejercicio1.go
@@ -0,0 +1,14 @@
+/*
+Punteros, funciones y structs
+A. Crear una función simple que permita multiplicar dos números y
+devolver el resultado.
+B. Crear una función que reciba un número variable de enteros como
+argumento y devuelva el menor de ellos.
+C. Crear una estructura Animal, y varias estructuras que tomen sus
+propiedades y representen diferentes tipos de animales. Crear
+además una función que permita instanciar un Animal y devuelva un
+puntero al mismo.
+D. Crear un a función que permita obtener de forma recursiva el
+factorial de un número.
+*/
+
